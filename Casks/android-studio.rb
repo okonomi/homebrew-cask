e6b7f79adde35aceaf30984ab6903a4de4242c35
okonomi <oknm@okonomi.sakura.ne.jp>
2015-02-19 15:19:53 +0900

    Update Android Studio to 1.1

--- Casks/android-studio.rb
+++ Casks/android-studio.rb
@@ -1,9 +1,9 @@
 cask :v1 => 'android-studio' do
-  version '1.0.2'
-  sha256 'bfbbf184997bb50d14e29efdab89935118bb1dc9c3a4901396782011d21cd797'
+  version '1.1.0'
+  sha256 '7d31edf46f57c2be12e228fbdb2344c0fb3a4e3fc07b692ec05156c575ef7a61'
 
   # google.com is the official download host per the vendor homepage
-  url "https://dl.google.com/dl/android/studio/ide-zips/#{version}/android-studio-ide-135.1653844-mac.zip"
+  url "https://dl.google.com/dl/android/studio/ide-zips/#{version}/android-studio-ide-135.1740770-mac.zip"
   name 'Android Studio'
   homepage 'https://developer.android.com/sdk/'
   license :apache
@@ -11,7 +11,7 @@ cask :v1 => 'android-studio' do
   app 'Android Studio.app'
 
   caveats <<-EOS.undent
-    If you have Java 7 or above installed, you may want to use it as Android Studio JDK, for example:
+    If you have Java 8 or above installed, you may want to use it as Android Studio JDK, for example:
 
     export STUDIO_JDK=/Library/Java/JavaVirtualMachines/jdk1.8.0_25.jdk
 
